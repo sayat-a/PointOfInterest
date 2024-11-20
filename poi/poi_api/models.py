02adda70739ee66b67b1cd6d0c856dd14c40a493
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-20 13:37:25 +0500

    update poi/poi_api/models.py

--- poi/poi_api/models.py
+++ poi/poi_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# Создаем модель для точки интереса
+class PointOfInterest(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
